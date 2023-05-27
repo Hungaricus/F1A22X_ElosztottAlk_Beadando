6d85f1a3a1ba822b334294a189b9984d70e0c4cb
Hungaricus <45882320+MarcellHUN@users.noreply.github.com>
2023-05-27 20:59:14 +0200

    Game class javítása, hogy megfelelően legyen a harc helye kijelezve

--- src/Game.java
+++ src/Game.java
@@ -11,20 +11,22 @@ public class Game {
         while (warrior.getHealth() > 0 && wizard.getHealth() > 0) {
             warrior.move();
             wizard.move();
-            if (warrior.getPosition() == wizard.getPosition()) {
-                warrior.fight(wizard);
-                System.out.println("X__ --> harc: H:" + warrior.getHealth() + ", V:" + wizard.getHealth());
-            } else {
-                String positions = "";
-                for (int i = 0; i < 3; i++) {
-                    if (warrior.getPosition() == i) {
-                        positions += "H";
-                    } else if (wizard.getPosition() == i) {
-                        positions += "V";
-                    } else {
-                        positions += "_";
-                    }
+            String positions = "";
+            for (int i = 0; i < 3; i++) {
+                if (warrior.getPosition() == i && wizard.getPosition() == i) {
+                    positions += "X";  // both at the same position
+                } else if (warrior.getPosition() == i) {
+                    positions += "H";
+                } else if (wizard.getPosition() == i) {
+                    positions += "V";
+                } else {
+                    positions += "_";
                 }
+            }
+            if(warrior.getPosition() == wizard.getPosition()) {
+                warrior.fight(wizard);
+                System.out.println(positions + " --> harc: H:" + warrior.getHealth() + ", V:" + wizard.getHealth());
+            } else {
                 System.out.println(positions + " --> H:" + warrior.getHealth() + ", V:" + wizard.getHealth());
             }
         }
