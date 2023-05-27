10b112ee50cbdc69577ffcc852b1f2a3beab0e36
Hungaricus <45882320+MarcellHUN@users.noreply.github.com>
2023-05-27 21:24:26 +0200

    Kivétel teljes

--- src/Game.java
+++ src/Game.java
@@ -8,27 +8,39 @@ public class Game {
     }
 
     public void play() {
-        while (warrior.getHealth() > 0 && wizard.getHealth() > 0) {
-            warrior.move();
-            wizard.move();
-            String positions = "";
-            for (int i = 0; i < 3; i++) {
-                if (warrior.getPosition() == i && wizard.getPosition() == i) {
-                    positions += "X";  // both at the same position
-                } else if (warrior.getPosition() == i) {
-                    positions += "H";
-                } else if (wizard.getPosition() == i) {
-                    positions += "V";
+        try {
+            while (warrior.getHealth() > 0 && wizard.getHealth() > 0) {
+                warrior.move();
+                wizard.move();
+                String positions = "";
+                for (int i = 0; i < 3; i++) {
+                    if (warrior.getPosition() == i && wizard.getPosition() == i) {
+                        positions += "X";  // mindkettő ugyan ott van és harcol
+                    } else if (warrior.getPosition() == i) {
+                        positions += "H";
+                    } else if (wizard.getPosition() == i) {
+                        positions += "V";
+                    } else {
+                        positions += "_";
+                    }
+                }
+                if (warrior.getPosition() == wizard.getPosition()) {
+                    warrior.fight(wizard);
+                    System.out.println(positions + " --> harc: H:" + warrior.getHealth() + ", V:" + wizard.getHealth());
                 } else {
-                    positions += "_";
+                    System.out.println(positions + " --> H:" + warrior.getHealth() + ", V:" + wizard.getHealth());
+                }
+
+                if (warrior.getHealth() <= 0 && wizard.getHealth() <= 0) {
+                    throw new GameOverException("Döntetlen! Dühükben elpusztították egymást!");
+                } else if (warrior.getHealth() <= 0) {
+                    throw new GameOverException("A bölcs varázsló nyert!");
+                } else if (wizard.getHealth() <= 0) {
+                    throw new GameOverException("A hatalmas harcos nyert!");
                 }
             }
-            if(warrior.getPosition() == wizard.getPosition()) {
-                warrior.fight(wizard);
-                System.out.println(positions + " --> harc: H:" + warrior.getHealth() + ", V:" + wizard.getHealth());
-            } else {
-                System.out.println(positions + " --> H:" + warrior.getHealth() + ", V:" + wizard.getHealth());
-            }
+        } catch (GameOverException e) {
+            System.out.println(e.getMessage());
         }
     }
 
---
+++ src/GameOverException.java
@@ -0,0 +1,5 @@
+class GameOverException extends Exception {
+    public GameOverException(String message) {
+        super(message);
+    }
+}
